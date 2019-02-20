a8ecb307ad6ccca952d87e457258fde9ec73ac8e
CarlosW1998 <carlos-walte@hotmail.com>
2019-02-19 23:20:03 -0300

    Agentes

---
+++ Pacman/agents.py
@@ -0,0 +1,22 @@
+import math
+import random
+import arcade
+from states import AgentState
+
+class Pacman:
+    def __init__(self, center_x, center_y, velocity):
+        self.x = center_x
+        self.y = center_y
+        self.velocity = velocity
+    
+    def move(self, dt=0):pass        
+
+
+class Gosth:
+    def __init__(self, center_x, center_y, velocity):
+        self.x = center_x
+        self.y = center_y
+        self.velocity - velocity
+        self.state = None
+
+    def move(self, dt=0):pass        
--- Pacman/states.py
+++ Pacman/states.py
@@ -5,3 +5,7 @@ class State(Enum):
     MAIN_MENU = 0
     PLAYING = 1
     GAME_OVER = 2
+
+class AgentState(Enum):
+    SEARCH = 0
+    STAL = 1
