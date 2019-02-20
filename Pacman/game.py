2979fef3e66440a72e527f3482284ba6213756a3
CarlosW1998 <carlos-walte@hotmail.com>
2019-02-19 23:02:37 -0300

    main menu

--- Pacman/game.py
+++ Pacman/game.py
@@ -1,28 +1,29 @@
 import arcade
 from maze import make_maze
+from states import State
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+SCREEN_TITLE = "Pacman"
 
 
 class MyGame(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-
         self.size_block = 20
         self.maze = make_maze(width//(self.size_block*3), height//(self.size_block*2))
-        arcade.set_background_color(arcade.color.BLACK_OLIVE)
 
     def setup(self):
         # Create your sprites and sprite lists here
-        pass
+        self.state = State.MAIN_MENU
+        arcade.set_background_color(arcade.color.BLACK_OLIVE)
+
     def drow_maze(self):
         for i in range(len(self.maze)):
             for j in range(len(self.maze[i])):
                 if self.maze[i][j] == 1:
-                    arcade.draw_point(j*self.size_block, i*self.size_block, arcade.color.RED, self.size_block)
+                    arcade.draw_point(j*self.size_block, i*self.size_block, arcade.color.BLUE_BELL, self.size_block)
 
     def on_draw(self):
         """
@@ -32,7 +33,10 @@ class MyGame(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
-        self.drow_maze()
+        if self.state == State.MAIN_MENU:
+            arcade.draw_text("Press ENTER to play", 3, 405, arcade.color.BLACK, 50)
+        if self.state == State.PLAYING:
+            self.drow_maze()
 
         # Call draw() on all your sprite lists below
 
@@ -51,7 +55,11 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         http://arcade.academy/arcade.key.html
         """
-        pass
+
+        if key == arcade.key.SPACE and self.state == State.MAIN_MENU:
+            # If the game is starting, just change the state and return
+            self.state = State.PLAYING
+            return
 
     def on_key_release(self, key, key_modifiers):
         """
