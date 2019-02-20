a2c71370fce949a3ee67333ce8aa0c0fa7d9f6e2
CarlosW1998 <carlos-walte@hotmail.com>
2019-02-19 23:22:00 -0300

    bugs

--- Pacman/agents.py
+++ Pacman/agents.py
@@ -16,7 +16,7 @@ class Gosth:
     def __init__(self, center_x, center_y, velocity):
         self.x = center_x
         self.y = center_y
-        self.velocity - velocity
+        self.velocity = velocity
         self.state = None
 
     def move(self, dt=0):pass        
